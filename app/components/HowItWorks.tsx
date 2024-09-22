a4a335b58f5686b52bf5e62d556425f3b5197641
nobody <181721587+lesueurii@users.noreply.github.com>
2024-09-22 10:00:45 +1000

    update explanation

--- app/components/HowItWorks.tsx
+++ app/components/HowItWorks.tsx
@@ -32,7 +32,7 @@ export default function HowItWorks() {
                         4. Check the leaderboard to see your rank
                     </p>
                     <p className="py-2 text-left max-w-md">
-                        Only the last two hours of your listening activity is recorded. Make sure to revisit this site often to update your stats.
+                        Only the last two hours of your recent 50 Spotify streams are recorded. Make sure to revisit this site every 2 hours to update your stats.
                     </p>
                     <p className="py-2 text-left">
                         <b>
